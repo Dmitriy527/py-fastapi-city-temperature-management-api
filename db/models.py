dd31ee04edaa89e968dbebb6bf4daa8eef585cf0
Dmytro <dimkanividimka4@gmail.com>
2026-09-16 10:22:19 +0300

    Create model DBTemperature

--- db/models.py
+++ db/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+import datetime
+
+from sqlalchemy import String, ForeignKey, DateTime, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.engine import Base
 
@@ -9,4 +11,13 @@ class DBCity(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
     name: Mapped[str] = mapped_column(String(255), nullable = False,unique=True)
-    additional_info: Mapped[str] = mapped_column(String(1000), nullable = True)
+    additional_info: Mapped[str] = mapped_column(String(1000), nullable = True
+
+
+class DBTemperature(Base):
+        __tablename__ = "temperature"
+        id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
+        city_id: Mapped[int] = mapped_column(ForeignKey("city.id"), nullable = False)
+        city = relationship(DBCity)
+        date_time: Mapped[datetime] = mapped_column(DateTime, nullable = False)
+        temperature: Mapped[float] = mapped_column(Float, nullable = False)
